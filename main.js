416f1f454739c79504198a67989a345aa2c9cf95
luke-hoang <lh7190123@gmail.com>
2023-08-07 15:33:59 +1000

    Render search results on the page

---
+++ main.js
@@ -0,0 +1,160 @@
+search('keyboard');
+
+async function search(word) {
+  const endpoint = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+  const response = await fetch(endpoint);
+  
+  if (response.ok) {
+    const results = await response.json();
+    results.forEach(result => {
+      renderResult(result);
+    });
+  } else {
+    const error = await response.json();
+    renderError(error);
+  }
+}
+
+
+function renderError(error) {
+  const content = document.createElement('article');
+  
+  const heading = document.createElement('h2');
+  heading.textContent = error.title;
+  content.appendChild(heading);
+  
+  const paragraph = document.createElement('p');
+  paragraph.textContent = `${error.message} ${error.resolution}`;
+  content.appendChild(paragraph);
+
+  document.body.appendChild(content);
+}
+
+
+function renderResult(result) {
+  const content = document.createElement('article');
+  
+  const word = document.createElement('h2');
+  word.textContent = result.word;
+  content.appendChild(word);
+
+  const phonetic = document.createElement('h3');
+  phonetic.textContent = result.phonetic;
+  content.appendChild(phonetic);
+
+  const meanings = renderMeanings(result.meanings);
+  content.appendChild(meanings);
+
+  const source = renderUrls(result.sourceUrls);
+  content.appendChild(source);
+
+  document.body.appendChild(content);
+}
+
+
+function renderMeanings(meanings) {
+  const content = document.createElement('section');
+
+  for (const meaning of meanings) {
+    const partOfSpeech = document.createElement('h4');
+    partOfSpeech.textContent = meaning.partOfSpeech;
+    content.appendChild(partOfSpeech);
+
+    const definitions = renderDefinitions(meaning.definitions);
+    content.appendChild(definitions);
+
+    if (meaning.synonyms.length > 0 ) {
+      const synoynms = renderSynonyms(meaning.synonyms);
+      content.appendChild(synoynms);
+    }
+
+    if (meaning.antonyms.length > 0) {
+      const antonyms = renderAntonyms(meaning.antonyms);
+      content.appendChild(antonyms);
+    }
+  }
+
+  return content;
+}
+
+
+function renderDefinitions(definitions) {
+  const content = document.createElement('section');
+  
+  const heading = document.createElement('h5');
+  heading.textContent = 'Meaning'
+  content.appendChild(heading);
+
+  const list = document.createElement('ul');
+
+  for (const obj of definitions) {
+    const item = document.createElement('li');
+
+    const definition = document.createElement('p');
+    definition.textContent = obj.definition;
+    item.appendChild(definition);
+
+    if (obj.hasOwnProperty('example')) {
+      const example = document.createElement('p');
+      example.textContent = obj.example;
+      item.appendChild(example);
+    }
+    
+    list.appendChild(item);
+  }
+
+  content.append(list);
+
+  return content;
+}
+
+
+function renderSynonyms(synonynms) {
+  const content = document.createElement('section');
+
+  const heading = document.createElement('h5');
+  heading.textContent = 'Synonyms';
+  content.appendChild(heading);
+
+  const paragraph = document.createElement('p');
+  paragraph.textContent = synonynms.join(', ');
+  content.appendChild(paragraph);
+
+  return content;
+}
+
+
+function renderAntonyms(antonynms) {
+  const content = document.createElement('section');
+
+  const heading = document.createElement('h5');
+  heading.textContent = 'Antonyms';
+  content.appendChild(heading);
+
+  const paragraph = document.createElement('p');
+  paragraph.textContent = antonynms.join(', ');
+  content.appendChild(paragraph);
+
+  return content;
+}
+
+
+function renderUrls(urls) {
+  const content = document.createElement('section');
+
+  const heading = document.createElement('h5');
+  heading.textContent = 'Sources';
+  content.appendChild(heading);
+
+  for (const url of urls) {
+    const anchor = document.createElement('a');
+    anchor.setAttribute('href', url);
+    anchor.setAttribute('target', '_blank');
+    anchor.textContent = url;
+    content.appendChild(anchor);
+  }
+
+  return content;
+}
+
+
