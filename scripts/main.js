db7010663ea0d3cc69b886b6388e6b881b6aff96
luke-hoang <lh7190123@gmail.com>
2023-08-18 22:43:31 +1000

    Add comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
-// load custom font and theme
+// load and store custom font and theme
 loadCustomSettings();
-
 const customSettings = getCustomSettings();
 
 
@@ -56,23 +55,29 @@ document
   });
 
 
-// display search results 
+// search for a word
 const searchForm = document.getElementById('search-form');
 searchForm.addEventListener('submit', (event) => {
   event.preventDefault();
 
+  // remove existing input error message prior to a new search attempt
   const inputError = document.getElementById('input-error');
   if (inputError !== null) {
     inputError.remove();
   }
 
+  // reset main section
   document.querySelector('main').innerHTML = '';
 
+  // check the input field
+  // if there is value, attempt a search
+  // else display an input error message
   const searchInput = document.getElementById('search-input');
   if (searchInput.value.trim().length > 0) {
     searchInput.removeAttribute('style');
     search(searchInput.value);
   } else {
+    // if the input is blank, then display an input error message
     const message = renderInputError();
     searchInput.style.outline = '1px solid #FF5252';
     searchForm.after(message);
